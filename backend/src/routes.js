f3ba24e627606bf0086b62ab2652f43457eed879
rodrigosakamoto <rodosakamoto@gmail.com>
2020-02-12 09:15:59 -0300

    models, loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rodrigo Sakamoto',
+    email: 'rodrigo@rocketseat.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
