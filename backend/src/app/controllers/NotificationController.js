54ae362dd88c213203af110cb81a64fd4bcf8df5
rodrigosakamoto <rodosakamoto@gmail.com>
2020-02-14 11:09:11 -0300

    Envio de notificações

---
+++ backend/src/app/controllers/NotificationController.js
@@ -0,0 +1,37 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+  // Lista notificações
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notification' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  // Marca notificação como lida
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+export default new NotificationController();
